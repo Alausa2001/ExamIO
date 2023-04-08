f1466558016c79a90e51462bf133c14dc3c7f499
Alausa Abdulqoyum Oluwaferanmi <101368817+Alausa2001@users.noreply.github.com>
2023-04-08 13:01:24 +0100

    Update db_setup.sql

--- db_setup.sql
+++ db_setup.sql
@@ -5,6 +5,6 @@
 SET GLOBAL validate_password.policy=LOW;
 CREATE DATABASE IF NOT EXISTS examio_db;
 CREATE USER IF NOT EXISTS 'examio'@'localhost' IDENTIFIED BY 'examio_v1.0';
-GRANT ALL PRIVILEGES ON salvator_db.* TO 'examio'@'localhost';
+GRANT ALL PRIVILEGES ON examio_db.* TO 'examio'@'localhost';
 GRANT SELECT ON performance_schema.* TO 'examio'@'localhost';
 FLUSH PRIVILEGES;
