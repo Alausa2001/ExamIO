b376867d1e0f6418850953c199c5e5e55eecb7c5
Alausa Abdulqoyum Oluwaferanmi <oluwaferanmialausa2001@gmail.com>
2023-04-15 18:25:55 +0100

    working o route that handles retreiving exam questions and submission of result

---
+++ backend/api/routes/index.js
@@ -0,0 +1,24 @@
+#!/usr/bin/node
+const express = require('express');
+const User = require('../controllers/user');
+const auth = require('../middlewares/auth');
+const Examination = require('../controllers/exam');
+
+const router = express.Router();
+
+/* get requests */
+router.get('/', (req, res) => {
+  res.status(200).json("Hello World");
+});
+router.get('/student/take-exam', auth, Examination.takeExam);
+
+/* post requests */
+router.post('/student/signup', User.studentSignUp);
+router.post('/student/signin', User.studentSignIn);
+router.post('/examiner/signup', User.examinerSignUp);
+router.post('/examiner/signin', User.examinerSignIn);
+
+router.post('/examiner/create-exam', auth, Examination.createExam);
+
+
+module.exports = router;
---
+++ backend/models/examiner.js
@@ -0,0 +1,93 @@
+#!/usr/bin/node
+const { Sequelize, DataTypes } = require('sequelize');
+const { mysqldb, sequelize } = require('./engine/db');
+
+class Examiner extends Sequelize.Model {}
+
+/* examiner model */
+Examiner.init({
+  firstName: {
+    type: DataTypes.STRING(255),
+    allowNull: false,
+  },
+  lastName: {
+    type: DataTypes.STRING(255),
+    allowNull: false,
+  },
+  password: {
+    type: DataTypes.STRING(255),
+    allowNull: false,
+  },
+  email: {
+    type: DataTypes.STRING(255),
+    unique: true,
+    allowNull: false,
+  },
+  examinerId: {
+    type: DataTypes.UUID,
+    defaultValue: DataTypes.UUIDV4,
+    primaryKey: true,
+    unique: true,
+  },
+}, {
+  sequelize,
+  tableName: 'examiner',
+  modelName: 'Examiner',
+  timestamp: true,
+});
+
+/* Exam Records */
+class ExaminerRecords extends Sequelize.Model {}
+
+ExaminerRecords.init({
+  duration: {
+    type: DataTypes.STRING(255),
+    allowNull: true,
+  },
+  course: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  examId: {
+    type: DataTypes.UUID,
+    defaultValue: DataTypes.UUIDV4,
+    allowNull: false,
+  },
+}, {
+  sequelize,
+  tableName: 'examiner_records',
+  modelName: 'ExaminerRecords',
+  timestamp: true,
+});
+
+/* Examine and ExaminerRecords relationship (one-to-many) */
+
+Examiner.hasMany(ExaminerRecords);
+ExaminerRecords.belongsTo(Examiner, {
+  foreignKey: {
+    name: 'examinerId',
+    allowNull: false,
+    onDelete: 'SET NULL',
+    onUpdate: 'CASCADE',
+  },
+});
+
+async function create() {
+  await mysqldb.createTables();
+  // await sequelize.sync({ force: true });
+  let examiner = Examiner.build({firstName: 'wale', lastName: 'adebisi', email: 'wale@gmail', password: 'adenuga2001'});
+  examiner = await mysqldb.save(examiner);
+
+
+
+
+  const exam = ExaminerRecords.build({ duration: '120 mins', course: 'math', examinerId: examiner.examinerId})
+  const r = await mysqldb.save(exam);
+  const n = await mysqldb.update(exam, {course: 'bio'});
+
+  console.log(examiner);
+  console.log(r);
+}
+//create();
+
+module.exports = { Examiner, ExaminerRecords };
