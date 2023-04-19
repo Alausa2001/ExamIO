380c8a1a63c13cc043411e6095fa1afa499dbc10
Alausa Abdulqoyum Oluwaferanmi <oluwaferanmialausa2001@gmail.com>
2023-04-19 15:32:56 +0100

    enabled cors

--- backend/api/server.js
+++ backend/api/server.js
@@ -1,11 +1,13 @@
 #!/usr/bin/node
 
 const express = require('express');
+const cors = require('cors');
 const router = require('./routes/index');
 
 const app = express();
 //app.set('json spaces', 4)
 app.use(express.json());
+app.use(cors());
 
 app.use('/api', router);
 
