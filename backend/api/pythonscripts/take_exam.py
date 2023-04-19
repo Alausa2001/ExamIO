78ed58508c8a1d823a6e8129f253adcaf50c60c3
Alausa Abdulqoyum Oluwaferanmi <oluwaferanmialausa2001@gmail.com>
2023-04-19 18:50:06 +0100

    take exam

--- backend/api/pythonscripts/take_exam.py
+++ backend/api/pythonscripts/take_exam.py
@@ -5,14 +5,14 @@ def signup():
     """ student signup  """
     header = {'Content-Type': 'application/json'}
     data = {'firstname': 'Abdulqoyum', 'lastname': 'Abdulqoyum', 'email': 'Abdulqoyum@ymail', 'password': 'Abdulqoyum001'}
-    res = requests.post('http://localhost:3000/api/student/signup', headers=header, json=data)
+    res = requests.post('http://api.examio.feranmi.tech/api/student/signup', headers=header, json=data)
     print(res.json())
 
 def signin():
     """student signin"""
     header = {'Content-Type': 'application/json'}
     data = {'email': 'Abdulqoyum@ymail', 'password': 'Abdulqoyum001'}
-    res = requests.post('http://localhost:3000/api/student/signin', headers=header, json=data)
+    res = requests.post('http://api.examio.feranmi.tech/api/student/signin', headers=header, json=data)
     print(res.json())
     return res.headers.get('Authorization')
 
@@ -21,7 +21,7 @@ def takeexam(token):
 
     header = {'Authorization': token, 'Content-Type': 'application/json'}
     data = {'examId': '1f05ab02-50b4-408a-b413-f305dbd143b6', 'page': 2}
-    res = requests.get('http://localhost:3000/api/student/take-exam', headers=header, json=data)
+    res = requests.get('http://api.examio.feranmi.tech/api/student/take-exam', headers=header, json=data)
     print(res.json())
 
 if __name__ == "__main__":
